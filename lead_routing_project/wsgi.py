c0dcb89a4d88a0e05c9bbe6f8919a16fac8d6d6d
Karlo Untalan <karlountalan@Karlos-MacBook-Pro.local>
2021-03-04 01:02:54 +0800

    for deployment

--- lead_routing_project/wsgi.py
+++ lead_routing_project/wsgi.py
@@ -14,4 +14,7 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lead_routing_project.settings')
 sys.path.append('/home/leadscor/public_html/') #Add this also
 sys.path.append('/home/leadscor/public_html/lead_env/lib/python3.7/site-packages')
+activate_this = '/home/leadscor/public_html/lead_env/bin/activate_this.py'
+with open(activate_this) as file_:
+    exec(file_.read(), dict(__file__=activate_this))
 application = get_wsgi_application()
